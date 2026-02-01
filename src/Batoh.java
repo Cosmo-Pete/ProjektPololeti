cb7783b07aa087d0ed6b160486dd559609be6659
Kosmický Petr <petr.pospichal09@gmail.com>
2026-02-01 15:51:53 +0100

    přidán konstruktor

--- src/Batoh.java
+++ src/Batoh.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Batoh {
     private static final int MAX_KAPACITA = 5;
     private List<Predmet> predmety;
     private List<Karta> karty; // Karty se do limitu 5 nepočítají
+
+    public Batoh() {
+        this.predmety = new ArrayList<>();
+        this.karty = new ArrayList<>();
+    }
 
     public boolean pridejPredmet(Predmet p) {
         return false;
