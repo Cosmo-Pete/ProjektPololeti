3378c6a4651f523f717a8c84034109fea4c8cae3
Kosmický Petr <petr.pospichal09@gmail.com>
2026-02-01 15:02:53 +0100

    přidané gettery + konstruktor

--- src/Karta.java
+++ src/Karta.java
@@ -4,7 +4,30 @@ public class Karta {
     private int obrana;
     private int zivoty;
 
+    public Karta(String jmeno, int utok, int obrana, int zivoty) {
+        this.jmeno = jmeno;
+        this.utok = utok;
+        this.obrana = obrana;
+        this.zivoty = zivoty;
+    }
+
     public void vylepsiStatistiky() { /* Logika pro Moudrý strom */ }
 
     public void utociNa(Karta souper) { /* Logika souboje */ }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public int getUtok() {
+        return utok;
+    }
+
+    public int getObrana() {
+        return obrana;
+    }
+
+    public int getZivoty() {
+        return zivoty;
+    }
 }
