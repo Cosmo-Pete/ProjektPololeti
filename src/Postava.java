a96e842b44e760cac6ada18a9747a277c9bd49b1
Kosmický Petr <petr.pospichal09@gmail.com>
2026-02-01 14:53:41 +0100

    přidané gettery a settery do postavy + konstruktor

--- src/Postava.java
+++ src/Postava.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Postava {
@@ -6,7 +7,47 @@ public class Postava {
     private boolean jeNepritel;
     private List<Karta> balicekKaret;
 
+    public Postava(String jmeno, String dialog, boolean jeNepritel) {
+        this.jmeno = jmeno;
+        this.dialog = dialog;
+        this.jeNepritel = jeNepritel;
+        this.balicekKaret = new ArrayList<>();
+    }
+
     public String promluv() {
-        return "";
+        return dialog;
+    }
+
+    // Gettery a settery
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    public void setJmeno(String jmeno) {
+        this.jmeno = jmeno;
+    }
+
+    public String getDialog() {
+        return dialog;
+    }
+
+    public void setDialog(String dialog) {
+        this.dialog = dialog;
+    }
+
+    public boolean isJeNepritel() {
+        return jeNepritel;
+    }
+
+    public void setJeNepritel(boolean jeNepritel) {
+        this.jeNepritel = jeNepritel;
+    }
+
+    public List<Karta> getBalicekKaret() {
+        return balicekKaret;
+    }
+
+    public void setBalicekKaret(List<Karta> balicekKaret) {
+        this.balicekKaret = balicekKaret;
     }
 }
