e65a4f5daeb82c5ff78300d1df58fa3d9bdd4e83
Kosmický Petr <petr.pospichal09@gmail.com>
2026-02-01 16:51:58 +0100

    Přidán načítač světa využívající Gson pro jednoduché načítání herního světa z JSON souboru
    - Implementována třída NacitacSveta, která načítá svět ze souboru pomocí knihovny Gson + Svět se nyní načítá přímo do DTO tříd (SvetDTO, MistnostDTO, PredmetDTO, PostavaDTO, KartaDTO), čímž se eliminuje ruční parsování + Po načtení DTO se vytvářejí instanční objekty Mistnost, Predmet, Postava a Karta

--- src/NacitacSveta.java
+++ src/NacitacSveta.java
@@ -1,5 +1,59 @@
-public class NacitacSveta {
+import com.google.gson.Gson;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-    public void nactiZeSouboru(String cesta) {
+public class NacitacSveta {
+    public Hra nactiZeSouboru(String cesta) throws IOException {
+        Gson gson = new Gson();
+        // Načteme svět přímo do HraDTO
+        SvetDTO svetDTO = gson.fromJson(new FileReader(cesta), SvetDTO.class);
+
+        // Vytvoření místností
+        Map<String, Mistnost> mistnosti = Mistnost.vytvorMistnosti(svetDTO.mistnosti);
+
+        // Propojení východů
+        Mistnost.propojVychody(svetDTO.mistnosti, mistnosti);
+
+        // Vytvoření hry
+        Hra hra = new Hra();
+        hra.setAktualniMistnost(mistnosti.get(svetDTO.startovniMistnost));
+        hra.setBatoh(new Batoh());
+        return hra;
+    }
+
+    // DTO třída pro Gson
+    static class SvetDTO {
+        String startovniMistnost;
+        MistnostDTO[] mistnosti;
+    }
+
+    static class MistnostDTO {
+        String nazev;
+        String popis;
+        Map<String, String> vychody;
+        PredmetDTO[] predmety;
+        PostavaDTO postava;
+    }
+
+    static class PredmetDTO {
+        String nazev;
+        String popis;
+        boolean prenositelny;
+    }
+
+    static class PostavaDTO {
+        String jmeno;
+        String dialog;
+        boolean jeNepritel;
+        KartaDTO[] balicekKaret;
+    }
+
+    static class KartaDTO {
+        String jmeno;
+        int utok;
+        int obrana;
+        int zivoty;
     }
 }
