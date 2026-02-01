0dd545dbcbdb8b3fe52f882bc43e770457f261ca
Kosmický Petr <petr.pospichal09@gmail.com>
2026-02-01 15:18:14 +0100

    implementace příkazu jdi (smer se zadavá malými písmeny ale je jedno jestli bude s diakritikou či bez) + kontrola zda existuje východ v danném směru + přesun do nové místnosti

--- src/PrikazJdi.java
+++ src/PrikazJdi.java
@@ -1,7 +1,39 @@
 public class PrikazJdi implements IPrikaz {
     @Override
-    public String proved(String[] parametry, Hra hra) { /* Změní aktualniMistnost */ return ""; }
+    public String proved(String[] parametry, Hra hra) {
+        if (parametry == null || parametry.length == 0) {
+            return "Kam chceš jít? Musíš zadat směr (sever, jih, východ, západ).";
+        }
+
+        String smer = parametry[0].toLowerCase();
+        
+        
+        if (smer.equals("východ") || smer.equals("vychod")) {
+            smer = "vychod";
+        } else if (smer.equals("západ") || smer.equals("zapad")) {
+            smer = "zapad";
+        }
+
+        Mistnost aktualniMistnost = hra.getAktualniMistnost();
+        if (aktualniMistnost == null) {
+            return "Není nastavena aktuální místnost.";
+        }
+
+        Mistnost cilovaMistnost = aktualniMistnost.getVychod(smer);
+        
+
+        if (cilovaMistnost == null) {
+            return "Tímto směrem se nelze pohnout. Zůstáváš v " + aktualniMistnost.getNazev() + ".";
+        }
+
+
+        hra.setAktualniMistnost(cilovaMistnost);
+        return "Přesunul jsi se do " + cilovaMistnost.getNazev() + ".\n" + cilovaMistnost.getPopis();
+    }
+
     @Override
-    public String getNazev() { return "jdi"; }
+    public String getNazev() {
+        return "jdi";
+    }
 }
 
