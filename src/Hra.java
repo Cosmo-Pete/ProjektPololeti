424ff6ec7253b557a2bac92a34c71ffdf2930c99
Kosmický Petr <petr.pospichal09@gmail.com>
2026-02-01 16:55:23 +0100

    implementována třída Hra

--- src/Hra.java
+++ src/Hra.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Hra {
@@ -6,12 +7,52 @@ public class Hra {
     private boolean konecHry;
     private Map<String, IPrikaz> mapaPrikazu;
 
-    public void inicializujHru() { /* Načte svět přes NacitacSveta */ }
-    public String zpracujPrikaz(String radek) { /* Rozpozná příkaz a zavolá proved() */ }
-    public boolean jeKonec() { /* Vrací stav hry */ }
-    public void setKonecHry(boolean konec) { /* Nastaví konec */ }
-    public void zkontrolujVitezstvi() { /* Ověří, zda byl poražen boss v Bráně šampionů */ }
+    public Hra() {
+        this.konecHry = false;
+        this.mapaPrikazu = new HashMap<>();
+    }
 
-    // Gettery pro Batoh a Mistnost
+    public void inicializujHru() {
+        NacitacSveta nacitac = new NacitacSveta();
+        try {
+            Hra nactenaHra = nacitac.nactiZeSouboru("svet.gson");
+            if (nactenaHra != null) {
+                this.aktualniMistnost = nactenaHra.getAktualniMistnost();
+                this.batoh = nactenaHra.getBatoh();
+            }
+        } catch (Exception e) {
+            System.err.println("Chyba při načítání světa: " + e.getMessage());
+        }
+    }
+
+    public String zpracujPrikaz(String radek) { // Rozpozná příkaz a zavolá proved()
+        return "";
+    }
+
+    public boolean jeKonec() {
+        return konecHry;
+    }
+
+    public void setKonecHry(boolean konec) {
+        this.konecHry = konec;
+    }
+
+    public void zkontrolujVitezstvi() { // Ověří, zda byl poražen boss v Bráně šampionů }
+
+    public Mistnost getAktualniMistnost() {
+        return aktualniMistnost;
+    }
+
+    public void setAktualniMistnost(Mistnost aktualniMistnost) {
+        this.aktualniMistnost = aktualniMistnost;
+    }
+
+    public Batoh getBatoh() {
+        return batoh;
+    }
+
+    public void setBatoh(Batoh batoh) {
+        this.batoh = batoh;
+    }
 }
 
