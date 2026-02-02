8f8cf1f3699b626f663d84005aa221170dc404ec
Kosmický Petr <petr.pospichal09@gmail.com>
2026-02-02 19:47:40 +0100

    Implementace braní a pokládání předmětů

--- src/PrikazPoloz.java
+++ src/PrikazPoloz.java
@@ -1,11 +1,35 @@
 public class PrikazPoloz implements IPrikaz {
     @Override
-    public String proved(String[] parametry, Hra hra) { /* Spustí souboj mezi vyloženou kartou a NPC */
-        return "";
+    public String proved(String[] parametry, Hra hra) {
+        if (parametry == null || parametry.length == 0) {
+            return "Musíš zadat název předmětu, který chceš položit.";
+        }
+
+        String nazevPredmetu = parametry[0];
+        Mistnost aktualniMistnost = hra.getAktualniMistnost();
+        Batoh batoh = hra.getBatoh();
+        
+        if (aktualniMistnost == null) {
+            return "Není nastavena aktuální místnost.";
+        }
+
+        if (batoh == null) {
+            return "Batoh není dostupný.";
+        }
+
+        Predmet predmet = batoh.vyhodPredmet(nazevPredmetu);
+        
+        if (predmet == null) {
+            return "Předmět '" + nazevPredmetu + "' není v batohu.";
+        }
+
+        aktualniMistnost.vlozPredmet(predmet);
+        
+        return "Položil jsi předmět: " + predmet.getNazev() + " do místnosti.";
     }
 
     @Override
     public String getNazev() {
-        return "nazev itemu který jsme položili";
+        return "poloz";
     }
 }
--- src/PrikazVezmi.java
+++ src/PrikazVezmi.java
@@ -1,11 +1,42 @@
 public class PrikazVezmi implements IPrikaz {
     @Override
-    public String proved(String[] parametry, Hra hra) { /* Spustí souboj mezi vyloženou kartou a NPC */
-        return "";
+    public String proved(String[] parametry, Hra hra) {
+        if (parametry == null || parametry.length == 0) {
+            return "Musíš zadat název předmětu, který chceš vzít.";
+        }
+
+        String nazevPredmetu = parametry[0];
+        Mistnost aktualniMistnost = hra.getAktualniMistnost();
+        Batoh batoh = hra.getBatoh();
+        
+        if (aktualniMistnost == null) {
+            return "Není nastavena aktuální místnost.";
+        }
+
+        if (batoh == null) {
+            return "Batoh není dostupný.";
+        }
+
+        Predmet predmet = aktualniMistnost.odeberPredmet(nazevPredmetu);
+        
+        if (predmet == null) {
+            return "Předmět '" + nazevPredmetu + "' není v této místnosti.";
+        }
+
+        if (!predmet.isPrenositelny()) {
+            return "Předmět '" + nazevPredmetu + "' není přenositelný.";
+        }
+
+        if (!batoh.pridejPredmet(predmet)) {
+            aktualniMistnost.vlozPredmet(predmet); // Vrátí předmět zpět, pokud se nepodařilo přidat do batohu
+            return "Batoh je plný. Nemůžeš vzít předmět '" + nazevPredmetu + "'.";
+        }
+
+        return "Vzal jsi předmět: " + predmet.getNazev() + ".";
     }
 
     @Override
     public String getNazev() {
-        return "nazev itemu který jsme vzali";
+        return "vezmi";
     }
 }
