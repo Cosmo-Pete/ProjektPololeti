5b14a760fff88dd79a26206da2c5b310895b5886
Kosmický Petr <petr.pospichal09@gmail.com>
2026-02-02 20:29:41 +0100

    Implemetován příkaz k útoku hráč si může vybrat kterou kartu vyloží a pak jde proti 1. kartě v balícku nepřítele momentálně mají všichni pouze 1 kartu

--- src/PrikazUtok.java
+++ src/PrikazUtok.java
@@ -1,7 +1,60 @@
 public class PrikazUtok implements IPrikaz {
     @Override
-    public String proved(String[] parametry, Hra hra) { /* Spustí souboj mezi vyloženou kartou a NPC */
-        return "";
+    public String proved(String[] parametry, Hra hra) {
+        if (parametry == null || parametry.length == 0) {
+            return "Musíš zadat název karty, kterou chceš vyložit pro boj.";
+        }
+
+        String nazevKarty = parametry[0];
+        Batoh batoh = hra.getBatoh();
+        Mistnost aktualniMistnost = hra.getAktualniMistnost();
+        
+        if (batoh == null) {
+            return "Batoh není dostupný.";
+        }
+
+        if (aktualniMistnost == null) {
+            return "Není nastavena aktuální místnost.";
+        }
+
+        Karta karta = batoh.najdiKartu(nazevKarty);
+        
+        if (karta == null) {
+            return "Karta '" + nazevKarty + "' není v batohu.";
+        }
+
+        Postava postava = aktualniMistnost.getPostava();
+        
+        if (postava == null) {
+            return "V této místnosti není žádný nepřítel.";
+        }
+
+        if (!postava.isJeNepritel()) {
+            return "Tato postava není nepřítelem.";
+        }
+
+        if (postava.getBalicekKaret().isEmpty()) {
+            return "Nepřítel nemá žádné karty.";
+        }
+
+        Karta nepritelovaKarta = postava.getBalicekKaret().get(0);
+        
+        // Boj mezi kartami
+        karta.utociNa(nepritelovaKarta);
+        
+        if (!nepritelovaKarta.jeZiva()) {
+            postava.getBalicekKaret().remove(nepritelovaKarta);
+            return "Porazil jsi nepřítele! Karta '" + karta.getJmeno() + "' porazila kartu '" + nepritelovaKarta.getJmeno() + "'.";
+        }
+
+        nepritelovaKarta.utociNa(karta);
+        
+        if (!karta.jeZiva()) {
+            batoh.odeberKartu(karta);
+            return "Nepřítel porazil tvou kartu '" + karta.getJmeno() + "'.";
+        }
+
+        return "Boj pokračuje. Karta '" + karta.getJmeno() + "' má " + karta.getZivoty() + " životů.";
     }
 
     @Override
