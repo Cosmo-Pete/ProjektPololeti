b029fcfe73f19d8bb2ff8cf7e75cedd2c993c5ad
Kosmický Petr <petr.pospichal09@gmail.com>
2026-02-02 16:28:27 +0100

    implementován příikaz mluv a kontrola zda-li je v místnosti a je tam postava se kterou lze mluvit

--- src/PrikazMluv.java
+++ src/PrikazMluv.java
@@ -1,11 +1,21 @@
 public class PrikazMluv implements IPrikaz {
     @Override
-    public String proved(String[] parametry, Hra hra) { /* Spustí souboj mezi vyloženou kartou a NPC */
-        return "";
+    public String proved(String[] parametry, Hra hra) {
+        Mistnost aktualniMistnost = hra.getAktualniMistnost();
+        if (aktualniMistnost == null) {
+            return "Není nastavena aktuální místnost.";
+        }
+
+        Postava postava = aktualniMistnost.getPostava();
+        if (postava == null) {
+            return "V této místnosti není nikdo, s kým bys mohl mluvit.";
+        }
+
+        return postava.getJmeno() + " říká: " + postava.promluv();
     }
 
     @Override
     public String getNazev() {
-        return "mluvený text";
+        return "mluv";
     }
 }
