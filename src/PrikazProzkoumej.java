4c64394e240077e28c28b3dec0d569f81554f7ec
Kosmický Petr <petr.pospichal09@gmail.com>
2026-02-02 16:31:03 +0100

    přidána kontrola zda-li je v místnosti a pokud ano tak mu vypise popis

--- src/PrikazProzkoumej.java
+++ src/PrikazProzkoumej.java
@@ -1,11 +1,16 @@
 public class PrikazProzkoumej implements IPrikaz {
     @Override
-    public String proved(String[] parametry, Hra hra) { /* Spustí souboj mezi vyloženou kartou a NPC */
-        return "";
+    public String proved(String[] parametry, Hra hra) {
+        Mistnost aktualniMistnost = hra.getAktualniMistnost();
+        if (aktualniMistnost == null) {
+            return "Není nastavena aktuální místnost.";
+        }
+
+        return aktualniMistnost.dlouhyPopis();
     }
 
     @Override
     public String getNazev() {
-        return "co jsme zjistili";
+        return "prozkoumej";
     }
 }
